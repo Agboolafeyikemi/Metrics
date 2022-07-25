cc203f821ad5e64919036b496af48839cd644585
Agboola Feyikemi <agboolafeyikemi@gmail.com>
2022-07-25 16:26:05 +0100

    Genesis Commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders the landing page", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByRole("heading")).toHaveTextContent(/Ad Testing Metrics/);
+  // expect(screen.getByRole("search")).toHaveDisplayValue("Search By");
+  // expect(screen.getByRole("sortBy")).toHaveDisplayValue("Sort By");
+  // expect(screen.getByRole("search", { name: "Search" })).toHaveClass;
+  // expect(screen.getByRole("img")).toBeInTheDocument();
 });
